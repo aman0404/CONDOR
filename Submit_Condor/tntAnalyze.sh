089be561b50a1d61e61ba01dcc0a0a0ae09f096d
florez <andres.florez@cern.ch>
2015-06-30 12:45:28 -0500

    changing tntAnalyze.sh

--- Submit_Condor/tntAnalyze.sh
+++ Submit_Condor/tntAnalyze.sh
@@ -20,20 +20,29 @@ date
 # CMSSW area. 
 #Also, please read the carefully the comments below.
 
-cd /uscms_data/d2/florez/TagAndProbe_BSM3G_TNT_Analyzer/CMSSW_7_4_1/src
+# "cd" to the are where you have installed your CMSSW release, e.g:
+# cd /uscms_data/d2/florez/TagAndProbe_BSM3G_TNT_Analyzer/CMSSW_7_4_1/src
+
+cd CHANGE_THIS_FOR_PATH_TO_YOUR_CMSSW_SRC_AREA
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scram runtime -sh`
 echo $_CONDOR_SCRATCH_DIR
 cd ${_CONDOR_SCRATCH_DIR}
-cp -r /uscms_data/d2/florez/TagAndProbe_BSM3G_TNT_Analyzer/CMSSW_7_4_1/src/Condor .
-cd Condor
+
+# Copy the directory where you have the compiled code, e.g.:
+# cp -r /uscms_data/d2/florez/TagAndProbe_BSM3G_TNT_Analyzer/CMSSW_7_4_1/src/Fermilab_TauHAT2015/muonToTauFakeRate . 
+cp -r CHANGE_THIS_FOR_PATH_TO_YOUR_ANALYSIS_CODE .
+
+# "cd" in to the analysos code directory, e.g:
+# cd muonToTauFakeRate
+cd CHANGE_THIS_FOR_ANALYSIS_DIRECTORY
 ./BSM_Analysis $fname $outputfile
 
 echo "LIST BEFORE MOVING"
 ls ${_CONDOR_SCRATCH_DIR}
 
 # Also, and very important: You need to create directories in eos
-# with matching names to those is the lists (WJets_HT100To600, WJets_HT200To400, etc)
+# with matching names to those is the lists (Ntuples_DYtoLL_Spring15)
 # The reason why I am sending the output to EOS is because someone told me at fermilab 
 # that when submmiting a large number of jobs we can saturate the EOS system by 
 # copying files directly and no with the xrdcp convention, which I think 
@@ -41,10 +50,13 @@ ls ${_CONDOR_SCRATCH_DIR}
 # if you can to copy the files directly, you can modify the line below
 # but you get yell at, I warned you :) 
 
-xrdcp $_CONDOR_SCRATCH_DIR/Condor/$outputfile  root://cmseos.fnal.gov//store/user/florez/TNT_Analyzer_Condor/$outputdir
+#Copy the output to your EOS area, e.g:
+#xrdcp $_CONDOR_SCRATCH_DIR/muonToTauFakeRate/$outputfile  root://cmseos.fnal.gov//store/user/florez/TNT_Analyzer_Condor/$outputdir
+
+xrdcp $_CONDOR_SCRATCH_DIR/CHANGE_THIS_FOR_ANALYSIS_DIRECTORY/$outputfile root://cmseos.fnal.gov//store/user/florez/YOUR_OUTPUT_DIRECTORY/$outputdir
 
 cd ${_CONDOR_SCRATCH_DIR}
-rm -rf Condor
+rm -rf CHANGE_THIS_FOR_ANALYSIS_DIRECTORY
 
 echo "List after moving/removing everything"
 ls ${_CONDOR_SCRATCH_DIR}
